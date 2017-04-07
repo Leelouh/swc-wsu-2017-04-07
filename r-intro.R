be0673035809c0dfcc2527aa168b5b56ebd911c8
Leelouh <laurahelou@yahoo.fr>
2017-04-07 11:04:07 -0700

    add R file

---
+++ r-intro.R
@@ -0,0 +1,18 @@
+1+100
+1==2
+x<-1/40
+x <- 1/40
+log(x)
+x+x
+x<-100
+x
+y <- c(1,4.5,2.7,9)
+y
+z <- 1:5
+animals <-c("monkey", "wolf", "girafe")
+animals
+y + 4
+
+paste("The animal is ", animals)
+new_vec <- c(5,7.5, "hat")
+new_vec
